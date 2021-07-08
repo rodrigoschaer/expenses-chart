38298346dd4a9a9bb7cfb7440740823fdaa5db57
Rodrigo Ferreira Schaer <rodrigo.fscs@gmail.com>
2021-07-08 15:16:00 -0300

    first commit

---
+++ src/components/ExpenseItem.tsx
@@ -0,0 +1,13 @@
+import "./ExpenseItem.scss";
+
+export function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
+}
