7635632f052d17651dee9c5591918c58307cb2ca
Rodrigo Ferreira Schaer <rodrigo.fscs@gmail.com>
2021-07-20 18:25:55 -0300

    added child bottom-up communitcation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import { ExpenseItem } from "./components/ExpenseItem";
+import { Expense } from "./components/Expenses/Expense";
+import { NewExpense } from "./components/NewExpense/NewExpense";
 
-function App() {
+export default function App() {
+  const expensesOptions = [
+    {
+      id: "expense1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "expense2",
+      title: "Amazon Books",
+      amount: 60.0,
+      date: new Date(2021, 2, 5),
+    },
+    {
+      id: "expense3",
+      title: "Agiota",
+      amount: 1000.67,
+      date: new Date(2021, 2, 13),
+    },
+    {
+      id: "expense4",
+      title: "Valorant",
+      amount: 100.0,
+      date: new Date(2021, 2, 22),
+    },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <NewExpense />
+      <Expense expenses={expensesOptions} />
     </div>
   );
 }
-
-export default App;
--- src/components/NewExpense/NewExpense.tsx
+++ src/components/NewExpense/NewExpense.tsx
@@ -1,25 +1,25 @@
 import React from "react";
+import { ExpenseForm } from "./ExpenseForm";
 import "./NewExpense.scss";
 
 export const NewExpense = () => {
+  type ExpenseData = {
+    title: string;
+    amount: number;
+    date: Date;
+  };
+
+  const handleSaveExpenseData = (enteredExpenseData: ExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <form action="">
-        <div className="new-expense__controls">
-          <div className="new-expense__control">
-            <label>Title</label>
-            <input type="text" />
-          </div>
-          <div className="new-expense__control">
-            <label>Amount</label>
-            <input type="number" />
-          </div>
-          <div className="new-expense__control">
-            <label>Date</label>
-            <input type="text" min="0.01" />
-          </div>
-        </div>
-      </form>
+      <ExpenseForm onSaveExpenseData={handleSaveExpenseData} />
     </div>
   );
 };
